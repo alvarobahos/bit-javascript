3a404517ea85849b1ceea04ecaf171a855dd0c24
alvarobahos <alvarobahosc@gmail.com>
2025-01-27 21:36:16 -0500

    datos dom

---
+++ script/script.js
@@ -0,0 +1,23 @@
+/*let estudiantes = [];
+estudiantes = [`alexandra`, `manuela`, `valentina`];
+console.log(estudiantes[2]);
+estudiantes[1] = `manuela alvarez`;*/
+
+const registormedico = {
+  nombrecompleto: `Casi miro`,
+  añodenacimiento: 2025,
+  documentodeidentidad: 123456789,
+  teléfonodecontacto: 55555,
+  personadecontacto: () => {
+    console.log(`Fulanito de Tal`);
+  },
+  motivoconsulta: () => {
+    console.log(`Dolor de oreja`);
+  },
+  hopitalizaciònreciente: true,
+  episidiosimilares: () => {
+    console.log(`Todos los dìas`);
+  },
+};
+
+console.log(registormedico);
